9c0c76848cd0ef9d6e879f6b9ced1cd788cc4614
Mayur Hanwate <70631103+lordbakyarou@users.noreply.github.com>
2023-09-27 23:29:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-const select = document.getElementById("colorSelect");
 
-const getButton = document.querySelector("form > input");
 
-getButton.addEventListener("click", (event) => {
-  for (let i = 0; i < select.children.length; i++) {
-    if (select.children[i].value === select.value) {
-      select.removeChild(select.children[i]);
-    }
-  }
-});
+
+
+
+function insert_Row(){}
