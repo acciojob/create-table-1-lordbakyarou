cf30b9275bcf10b0f1d6f6112e233383c00eb2dd
Mayur Hanwate <70631103+lordbakyarou@users.noreply.github.com>
2023-09-28 00:16:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,6 +7,11 @@ const td = table.children[0];
 function insert_Row(){
 
 	const tr = document.createElement("tr");
-	tr.innerHTML = "<td>New Cell1</td><td>New Cell2</td>"
+	const td1 = document.createElement("td");
+	const td2 = document.createElement("td");
+	td1.innerText = "New Cell1";
+	
+	td2.innerText = "New Cell2"
+	tr.append(td1,td2);
 	table.insertBefore(tr,td);
 }
