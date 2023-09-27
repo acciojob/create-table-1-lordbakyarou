9c19377d61ad27ad64719a31b95a36d989e902da
Mayur Hanwate <70631103+lordbakyarou@users.noreply.github.com>
2023-09-28 00:11:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 
+const table = document.getElementById("sampleTable");
+const td = table.children[0];
 
 
 
+function insert_Row(){
 
-function insert_Row(){}
+	const tr = document.createElement("tr");
+	tr.innerHTML = "<td>New Cell1</td><td>New Cell2</td>"
+	table.insertBefore(tr,td);
+}
